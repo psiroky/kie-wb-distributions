80fcbb5b826f928d689eaec4e4d51ea52499ae95
pefernan <pere.fernandez@gmail.com>
2014-04-16 17:55:44 +0200

    - Fixed errors on executing callback functions

--- kie-wb/kie-wb-webapp/src/main/webapp/js/jbpm-rest-integration.js
+++ kie-wb/kie-wb-webapp/src/main/webapp/js/jbpm-rest-integration.js
@@ -10,14 +10,11 @@ function jbpmRestAPI() {
 
             if (event.data == 'success' && lastConfig.onsuccess) lastConfig.onsuccess(event.data);
             else if (lastConfig.onerror) lastConfig.onerror(event.data);
-            delete configs[lastConfig.containerId];
         }
     }
 
     var getXMLDoc = function(xml) {
         if (!xml) return;
-
-        alert("the result xml: " + xml);
 
         var xmlDoc;
         if (window.DOMParser) {
@@ -60,14 +57,9 @@ function jbpmRestAPI() {
         ajaxHandler.ajaxResponse = function() {
             // Only if req shows "complete"
             var readyState, status;
-            try{
-                readyState = ajaxHandler.ajaxReq.readyState;
-                if (readyState == 4) {
-                    status = ajaxHandler.ajaxReq.status;
-                }
-            }
-            catch(e){
-                alert("error: " + e)
+            readyState = ajaxHandler.ajaxReq.readyState;
+            if (readyState == 4) {
+                status = ajaxHandler.ajaxReq.status;
             }
             if (readyState == 4) {
                 if (status == 200) {
@@ -83,57 +75,50 @@ function jbpmRestAPI() {
         ajaxHandler.ajaxReq.send();
     }
 
+    this.showStartProcessForm = function(hostUrl, deploymentId, processId, responseDiv, successCallback, errorCallback) {
 
-    this.setContentValues = function(containerId, message) {
-        try {
-            var frame = document.getElementById(containerId + '_restFrame');
-            if (frame) {
-                var frameDoc = frame.contentWindow.document;
-                frameDoc.getElementById('container').value = containerId;
-                frameDoc.getElementById('message').value = message;
-            }
-        } catch (err) {
-            alert(err);
-        }
+        if (!hostUrl || !deploymentId || !processId || !responseDiv) return;
 
-    }
+        if (hostUrl.charAt(hostUrl.length -1) != "/") hostUrl += "/";
 
-    this.startTaskForm = function(containerId, message) {
-        document.getElementById(containerId).innerHTML = '<iframe id="' + containerId + '_restFrame" src="content.html"/>';
-        var config = {
-            msg: message,
-            action: 'complete',
-            onsuccess: null,
-            onerror: null
-        };
-        configs[containerId] = config;
-        setTimeout("jbpmRestAPI.setContentValues('" + containerId + "', '" + message + "')", 100);
-    }
-
-    this.completeTaskForm = function(containerId, onsuccess, onerror) {
-        if (onsuccess != null) configs[containerId].onsuccess = onsuccess;
-        if (onerror != null) configs[containerId].onerror = onerror;
-        document.getElementById(containerId + '_restFrame').contentWindow.submitForm();;
-    }
-
-    this.showStartProcessForm = function(hostUrl, deploymentId, processId, responseDiv) {
         var config = {
             containerId: responseDiv,
             host: hostUrl,
             url: hostUrl + "rest/runtime/" + deploymentId + "/process/" + processId + "/startform",
-            status: null,
+            isProcess: true,
             deploymentId: deploymentId,
             processId: processId,
             formURL: null,
             onsuccess: function (responseText) {
                 var xmlDoc = getXMLDoc(responseText);
-                this.status = xmlDoc.getElementsByTagName("status")[0].childNodes[0].nodeValue;
-                this.formURL = xmlDoc.getElementsByTagName("formUrl")[0].childNodes[0].nodeValue;
-                var html = "<iframe id='" + this.containerId + "_form' src='" + this.formURL + "' frameborder='0' style='width:100%; height:100%'></iframe>";
-                var targetDiv = document.getElementById(this.containerId);
-                targetDiv.innerHTML = html;
+
+                if (!xmlDoc) {
+                    errorCallback(responseText);
+                    return;
+                }
+
+                var status = xmlDoc.getElementsByTagName("status");
+
+                if (status && status.length > 0 && status[0].childNodes.length > 0) {
+                    status = status[0].childNodes[0].nodeValue;
+
+                    if (status == 'SUCCESS') {
+                        var formURL = xmlDoc.getElementsByTagName("formUrl");
+                        if (formURL && formURL.length > 0 && formURL[0].childNodes.length > 0) {
+                            this.formURL = formURL[0].childNodes[0].nodeValue;
+                            var html = "<iframe id='" + this.containerId + "_form' src='" + this.formURL + "' frameborder='0' style='width:100%; height:100%'></iframe>";
+                            var targetDiv = document.getElementById(this.containerId);
+                            targetDiv.innerHTML = html;
+                            if (successCallback) successCallback(responseText);
+                            return;
+                        }
+                    }
+
+                }
+                if (errorCallback) errorCallback(responseText);
+
             },
-            onerror: null
+            onerror: errorCallback
         };
         configs[responseDiv] = config;
 
@@ -142,77 +127,93 @@ function jbpmRestAPI() {
 
 
     this.startProcess = function(responseDiv, onsuccess, onerror) {
-        postAction(responseDiv, 'startProcess', onsuccess, onerror);
+        var config = configs[responseDiv];
+        if (config && config.isProcess) postAction(config, 'startProcess', onsuccess, onerror);
     };
 
     this.claimTask = function(responseDiv, onsuccess, onerror) {
-        postAction(responseDiv, 'claimTask', onsuccess, onerror);
+        var config = configs[responseDiv];
+        if (config && !config.isProcess) postAction(config, 'claimTask', onsuccess, onerror);
     };
 
     this.startTask = function(responseDiv, onsuccess, onerror) {
-        postAction(responseDiv, 'startTask', onsuccess, onerror);
+        var config = configs[responseDiv];
+        if (config && !config.isProcess) postAction(config, 'startTask', onsuccess, onerror);
     };
 
     this.releaseTask = function(responseDiv, onsuccess, onerror) {
-        postAction(responseDiv, 'releaseTask', onsuccess, onerror);
+        var config = configs[responseDiv];
+        if (config && !config.isProcess) postAction(config, 'releaseTask', onsuccess, onerror);
     };
 
     this.saveTask = function(responseDiv, onsuccess, onerror) {
-        postAction(responseDiv, 'saveTask', onsuccess, onerror);
+        var config = configs[responseDiv];
+        if (config && !config.isProcess) postAction(config, 'saveTask', onsuccess, onerror);
     };
 
     this.completeTask = function(responseDiv, onsuccess, onerror) {
-        postAction(responseDiv, 'completeTask', onsuccess, onerror);
+        var config = configs[responseDiv];
+        if (config && !config.isProcess) postAction(config, 'completeTask', onsuccess, onerror);
     };
 
-    var postAction = function(responseDiv, action, onsuccess, onerror) {
-        if (responseDiv && action) {
-            var config = configs[responseDiv];
-            if (config) {
-                var frame = document.getElementById(responseDiv + '_form').contentWindow;
-                frame.postMessage(action, config.formURL);
-                lastConfig = config;
-                if (onsuccess) lastConfig.onsuccess = onsuccess;
-                if (onerror) lastConfig.onerror = onerror;
-            }
+    var postAction = function(config, action, onsuccess, onerror) {
+        if (config && action) {
+            var frame = document.getElementById(config.containerId + '_form').contentWindow;
+            frame.postMessage(action, config.formURL);
+            lastConfig = config;
+            if (onsuccess) lastConfig.onsuccess = onsuccess;
+            if (onerror) lastConfig.onerror = onerror;
         }
     }
 
-    this.showTaskForm = function (hostUrl, taskId, responseDiv) {
+    this.showTaskForm = function (hostUrl, taskId, responseDiv, successCallback, errorCallback) {
         var config = {
             containerId: responseDiv,
             host: hostUrl,
             url: hostUrl + "rest/task/" + taskId + "/showTaskForm",
-            status: null,
+            isProcess: false,
             formURL: null,
             onsuccess: function (responseText) {
                 var xmlDoc = getXMLDoc(responseText);
-                this.status = xmlDoc.getElementsByTagName("status")[0].childNodes[0].nodeValue;
-                this.formURL = xmlDoc.getElementsByTagName("formUrl")[0].childNodes[0].nodeValue;
-                var html = "<iframe id='" + this.containerId + "_form' src='" + this.formURL + "' frameborder='0' style='width:100%; height:100%'></iframe>";
-                var targetDiv = document.getElementById(this.containerId);
-                targetDiv.innerHTML = html;
+
+                if (!xmlDoc) {
+                    errorCallback(responseText);
+                    return;
+                }
+
+                var status = xmlDoc.getElementsByTagName("status");
+
+                if (status && status.length > 0 && status[0].childNodes.length > 0) {
+                    status = status[0].childNodes[0].nodeValue;
+
+                    if (status == 'SUCCESS') {
+                        var formURL = xmlDoc.getElementsByTagName("formUrl");
+                        if (formURL && formURL.length > 0 && formURL[0].childNodes.length > 0) {
+                            this.formURL = formURL[0].childNodes[0].nodeValue;
+                            var html = "<iframe id='" + this.containerId + "_form' src='" + this.formURL + "' frameborder='0' style='width:100%; height:100%'></iframe>";
+                            var targetDiv = document.getElementById(this.containerId);
+                            targetDiv.innerHTML = html;
+                            if (successCallback) successCallback(responseText);
+                            return;
+                        }
+                    }
+
+                }
+                if (errorCallback) errorCallback(responseText);
             },
-            onerror: null
+            onerror: errorCallback
         };
         configs[responseDiv] = config;
 
         getRequest(config);
     }
 
-
-    this.notifyError =  function (containerId) {
-        var onerror = configs[containerId].onerror;
-        if (onerror != null) onerror();
-    };
-
-    this.notifyOK =  function (containerId) {
-        var onsuccess = configs[containerId].onsuccess;
-        if (onsuccess != null) onsuccess();
-        document.getElementById(containerId).innerHTML = '';
-        delete configs[containerId];
-    };
+    this.clearContainer = function (containerId) {
+        if (containerId) {
+            delete configs[containerId];
+            document.getElementById(containerId).innerHTML = "";
+        }
+    }
 };
-
 
 var jbpmRestAPI = new jbpmRestAPI();
